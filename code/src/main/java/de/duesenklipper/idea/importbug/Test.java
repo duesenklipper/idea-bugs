a01cc9fe3da85303231e86f755358de4df3879cf
Carl-Eric Menzel <cmenzel@wicketbuch.de>
2014-10-09 15:39:42 +0200

    initial

---
+++ code/src/main/java/de/duesenklipper/idea/importbug/Test.java
@@ -0,0 +1,13 @@
+package de.duesenklipper.idea.importbug;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.isEmpty(""));
+    }
+}
+        
+        
+        
+        
